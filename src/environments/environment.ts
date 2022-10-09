fa70e6d70d5c50eec0f0b3c262352b0b78b1759c
Yura Lapitski <lyurik@tut.by>
2022-10-10 00:24:02 +0300

    fix: edit import link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://b4eddij4ge.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://vnpgfxqgkj.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://b4eddij4ge.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
