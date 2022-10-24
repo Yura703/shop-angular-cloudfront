0eece4b0202e27723b52e7b32389ed66b9419917
Yura Lapitski <lyurik@tut.by>
2022-10-24 22:02:54 +0300

    feat: add auth and localStore, add proccesing errors

--- src/app/core/interceptors/error-print.interceptor.ts
+++ src/app/core/interceptors/error-print.interceptor.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {
+  HttpErrorResponse,
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
@@ -19,13 +20,26 @@ export class ErrorPrintInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       tap({
-        error: () => {
+        error: (err: unknown) => {
           const url = new URL(request.url);
+          const status = (err as HttpErrorResponse).status;
 
-          this.notificationService.showError(
-            `Request to "${url.pathname}" failed. Check the console for the details`,
-            0
-          );
+          switch (status) {
+            case 401:
+              this.notificationService.showError('401: Not authorized', 0);
+              break;
+
+            case 403:
+              this.notificationService.showError('403. Access is forbidden', 0);
+              break;
+
+            default:
+              this.notificationService.showError(
+                `Request to "${url.pathname}" failed. Check the console for the details`,
+                0
+              );
+              break;
+          }
         },
       })
     );
